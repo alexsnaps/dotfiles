6d298cb28503b5db4a4e03c39d474c2ee663ba87
Alex Snaps <alex@wcgw.dev>
2025-02-24 18:27:27 -0500

    nvim: More telescope goodness
    Signed-off-by: Alex Snaps <alex@wcgw.dev>

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -24,6 +24,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
     map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+    map('<leader>fi', require('telescope.builtin').lsp_incoming_calls, '[F]ind [I]ncoming calls')
+    map('<leader>fo', require('telescope.builtin').lsp_outgoing_calls, '[F]ind [O]utgoing calls')
     map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
     map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
     map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  'nvim-telescope/telescope.nvim', branch = 'master',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require('telescope.builtin')
@@ -9,6 +9,13 @@ return {
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     vim.keymap.set('n', '<leader>fT', builtin.lsp_dynamic_workspace_symbols, { desc = 'Telescope tags' })
     vim.keymap.set('n', '<leader>ft', builtin.lsp_document_symbols, { desc = 'Telescope tags' })
+    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Telescope keymap' })
+    vim.keymap.set('n', '<leader>gc', builtin.git_bcommits, { desc = '[g]it buffer [c]ommits' })
+    vim.keymap.set('n', '<leader>gC', builtin.git_commits, { desc = '[g]it [C]ommits' })
+    vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = '[g]it [s]tatus' })
+    vim.keymap.set('x', '<leader>gc', function()
+      require('telescope.builtin').git_bcommits_range({from = s_start, to = s_end})
+    end, { desc = '[g]it buffer [c]ommits' })
   end,
 }
 

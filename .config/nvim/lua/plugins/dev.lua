b049c453afa35f5456de645aaf371815d5702e40
Alex Snaps <alex@wcgw.dev>
2025-02-27 04:51:23 -0500

    nvim: git-blame
    Signed-off-by: Alex Snaps <alex@wcgw.dev>

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -93,7 +93,7 @@ return {
     event = "VeryLazy",
     opts = {
       enabled = false,
-      message_template = " <summary> 󱇼  <date>   <author> <<sha>>",
+      message_template = " <summary> 󱇻  <date>   <author> <<sha>>",
       date_format = "%m-%d-%Y %H:%M",
       virtual_text_column = 40,
     },

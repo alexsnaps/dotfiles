d7b3eab0ef28a759987e758785efa0b0a58e1545
Alex Snaps <alex@wcgw.dev>
2025-02-27 04:50:10 -0500

    nvim: git-blame
    Signed-off-by: Alex Snaps <alex@wcgw.dev>

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -89,6 +89,19 @@ return {
     },
   },
   {
+    "f-person/git-blame.nvim",
+    event = "VeryLazy",
+    opts = {
+      enabled = false,
+      message_template = " <summary> 󱇼  <date>   <author> <<sha>>",
+      date_format = "%m-%d-%Y %H:%M",
+      virtual_text_column = 40,
+    },
+    keys = {
+      { "<leader>gb", function() require("gitblame").toggle() end, { desc = "[g]it [b]lame" } },
+    },
+  },
+  {
     'm4xshen/autoclose.nvim',
     opts = {
       options = {

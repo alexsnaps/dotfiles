dc5bf8d7c629027325b2ab6cbd1bb6def4444a6c
Alex Snaps <alex@wcgw.dev>
2025-12-10 17:57:45 -0500

    feat(nvim): fzf-lua
    Signed-off-by: Alex Snaps <alex@wcgw.dev>

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -207,4 +207,19 @@ return {
 			},
 		},
 	},
+	{
+		"ibhagwan/fzf-lua",
+		-- optional for icon support
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		-- or if using mini.icons/mini.nvim
+		-- dependencies = { "nvim-mini/mini.icons" },
+		---@module "fzf-lua"
+		---@type fzf-lua.Config|{}
+		---@diagnostics disable: missing-fields
+		opts = {},
+		---@diagnostics enable: missing-fields
+		config = function()
+			vim.keymap.set({ "v", "n" }, "<leader>rr", require("fzf-lua").lsp_code_actions)
+		end,
+	},
 }

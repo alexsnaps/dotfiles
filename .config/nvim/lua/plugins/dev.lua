1c05fc9baed38540ad66d3f354cb3f64746ab545
Alex Snaps <alex@wcgw.dev>
2026-03-04 08:30:06 -0500

    fix(nvim): Avoid double lsp attachment
    Signed-off-by: Alex Snaps <alex@wcgw.dev>

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -138,9 +138,9 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({})
-    end,
+    --config = function()
+    --  require("mason-lspconfig").setup({})
+    --end,
   },
   {
     "neovim/nvim-lspconfig",

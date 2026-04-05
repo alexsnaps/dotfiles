ae88b36f1b9d10998469c67c29ab23feed7d5d1e
Alex Snaps <alex@wcgw.dev>
2026-04-05 10:57:59 -0400

    feat(nvim): wire FzfLua in as ui select
    Signed-off-by: Alex Snaps <alex@wcgw.dev>

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -5,14 +5,14 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
   callback = function()
     -- check if treesitter has parser
---    if require("nvim-treesitter.parsers") then
-      -- use treesitter folding
---      vim.opt.foldmethod = "expr"
---      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
---    else
-      -- use alternative foldmethod
-      vim.opt.foldmethod = "syntax"
---    end
+    --    if require("nvim-treesitter.parsers") then
+    -- use treesitter folding
+    --      vim.opt.foldmethod = "expr"
+    --      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+    --    else
+    -- use alternative foldmethod
+    vim.opt.foldmethod = "syntax"
+    --    end
   end,
 })
 vim.opt.foldenable = false
@@ -21,3 +21,4 @@ vim.opt.textwidth = 120
 
 vim.g.rust_recommended_style = 0
 vim.g.have_nerd_font = true
+vim.cmd("FzfLua register_ui_select")
